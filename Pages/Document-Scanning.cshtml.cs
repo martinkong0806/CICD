363f351d0392c05107e16afd3634a688bf9f54c9
Martin Kong <martinkongkh@gmail.com>
2024-02-28 16:50:26 +0000

    home page update

---
+++ Pages/Document-Scanning.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CICD.Pages
+{
+    public class Document_ScanningModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
