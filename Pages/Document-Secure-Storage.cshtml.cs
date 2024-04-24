0ead416c884a0975ad7e63d7b128c89ea74a5c9d
Martin Kong <martinkongkh@gmail.com>
2024-04-24 15:20:33 +0100

    update

---
+++ Pages/Document-Secure-Storage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CICD.Pages
+{
+    public class Document_Secure_StorageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
